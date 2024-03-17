0ac2bae95a0be0db153645d31269044fec88a3ff
Aashish Anand <anandaashish512@gmail.com>
2024-03-18 00:08:10 +0530

    revive-page

---
+++ pages/revive.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/Header";
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage=()=>{
+    return(
+        <div className="flex "
+    )
+}
